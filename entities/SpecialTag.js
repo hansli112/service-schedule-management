68380c133cb8ebf75374c69d4e415c20fa943600
hansli112 <31886587+hansli112@users.noreply.github.com>
2025-12-10 18:13:53 +0800

    Create SpecialTag.js

---
+++ entities/SpecialTag.js
@@ -0,0 +1,17 @@
+{
+  "name": "SpecialTag",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string",
+      "description": "\u6a19\u7c64\u540d\u7a31\uff08\u5982\u8056\u9910\u3001\u611b\u9910\u3001\u5ba3\u6559\u4e3b\u65e5\uff09"
+    },
+    "color": {
+      "type": "string",
+      "description": "\u6a19\u7c64\u984f\u8272"
+    }
+  },
+  "required": [
+    "name"
+  ]
+}
